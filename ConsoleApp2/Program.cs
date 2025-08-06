73ee6373b3d2fdfa1000f1a8fe87f49acec805bc
HFGF <HFGF@DESKTOP-PALVK4B>
2025-08-06 09:17:04 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void starPrinter(int starRows)
+        {
+            for (int i = 1; i <= starRows; i++)
+            {
+                string starString = new string('*', i * 2 - 1);
+                Console.WriteLine(starString.PadLeft(starRows + i - 1));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            starPrinter(10);
+
+        }
+    }
+}
